83dca800edca005acbde485e2548f22bc2a20329
Justin Butler <57680670+JustinButler700@users.noreply.github.com>
2021-10-31 13:14:26 -0700

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,38 @@
+//Justin Butler
+/*
+26 / 26 test cases passed.
+Status: Accepted
+Runtime: 0 ms
+Memory Usage: 38.2 MB
+Runtime beats 100%. 
+*/
+// 10/31/2021 Happy Halloween!
+class Solution {
+    boolean first = true; // is true ONLY for the first element of the list
+    Node root; // Holds the head node of the newly constructed doubly linked list
+    Node current; // Holds the current node of the newly constructed doubly linked list
+    public Node flatten(Node head) {
+        if(head == null){return root;}
+        if(head !=null){
+            if(first) // Construct our doubly linked list
+            {
+                first = !first;
+                root = new Node(head.val);
+                current = root;
+            }
+            else // Map all values to the newly constructed list.
+            {
+                Node temp = current; // temp value to hold our prev element
+                current.next = new Node(head.val);
+                current = current.next;
+                current.prev = temp;
+            }
+        } 
+        if(head.child != null) // iterate over child nodes.
+        {
+            flatten(head.child);
+        }
+        if(head.next !=null){flatten(head.next);} // iterate next
+        return root;
+    }
+}
